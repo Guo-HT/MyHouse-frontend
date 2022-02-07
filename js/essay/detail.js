4472c36d750075c53ee6181f5d245b113675105f
Guo-HT <1622761893@qq.com>
2022-02-07 21:11:40 +0800

    finish1.0

--- js/essay/detail.js
+++ js/essay/detail.js
@@ -5,8 +5,8 @@ $(function () {
 
     // 点赞，点过-1，没点过+1
     $("#do_good").click(function () {
-        if($.cookie("is_login")!="true"){
-            location.href="/user/login.html";
+        if ($.cookie("is_login") != "true") {
+            location.href = "/user/login.html";
         }
         var paramsStr = window.location.search
         var params = new URLSearchParams(paramsStr)
@@ -29,7 +29,7 @@ $(function () {
                 "X-CSRFToken": get_csrf_token(),
             },
         }).done(function (msg) {
-            if(msg.state=="fail" && msg.msg=="jump to login"){
+            if (msg.state == "fail" && msg.msg == "jump to login") {
                 layer.msg("请登录");
                 return;
             }
@@ -40,46 +40,46 @@ $(function () {
         })
     })
 
-        // 收藏，点过-1，没点过+1
-        $("#collect").click(function () {
-            if($.cookie("is_login")!="true"){
-                location.href="/user/login.html";
+    // 收藏，点过-1，没点过+1
+    $("#collect").click(function () {
+        if ($.cookie("is_login") != "true") {
+            location.href = "/user/login.html";
+        }
+        var paramsStr = window.location.search
+        var params = new URLSearchParams(paramsStr)
+        var id = params.get('id') // list
+        if (!id) {
+            id = 1;
+        }
+        $.ajax({
+            url: app_root + "/essay/do_collect",
+            type: "post",
+            dataType: "json",
+            data: {
+                id: id,
+            },
+            xhrFields: {
+                withCredentials: true // 发送Ajax时，Request header中会带上 Cookie 信息。
+            },
+            crossDomain: true,
+            headers: {
+                "X-CSRFToken": get_csrf_token(),
+            },
+        }).done(function (msg) {
+            console.log(msg);
+            if (msg.state == "fail" && msg.msg == "jump to login") {
+                layer.msg("请登录");
+                return;
             }
-            var paramsStr = window.location.search
-            var params = new URLSearchParams(paramsStr)
-            var id = params.get('id') // list
-            if (!id) {
-                id = 1;
+            if (msg.msg.collect_state == "true") {
+                $("#collect_tip").text("已收藏");
+            } else {
+                $("#collect_tip").text("收藏");
             }
-            $.ajax({
-                url: app_root + "/essay/do_collect",
-                type: "post",
-                dataType: "json",
-                data: {
-                    id: id,
-                },
-                xhrFields: {
-                    withCredentials: true // 发送Ajax时，Request header中会带上 Cookie 信息。
-                },
-                crossDomain: true,
-                headers: {
-                    "X-CSRFToken": get_csrf_token(),
-                },
-            }).done(function (msg) {
-                console.log(msg);
-                if(msg.state=="fail" && msg.msg=="jump to login"){
-                    layer.msg("请登录");
-                    return;
-                }
-                if(msg.msg.collect_state=="true"){
-                    $("#collect_tip").text("已收藏");
-                }else{
-                    $("#collect_tip").text("收藏");
-                }
-            }).fail(function (e) {
-                console.log(e);
-            })
+        }).fail(function (e) {
+            console.log(e);
         })
+    })
 
     // 点击分享 链接拷贝至剪切板
     $("#share").click(function () {
@@ -97,64 +97,64 @@ $(function () {
     })
 
     // 搜索 跳转页面
-    $("#search_btn").click(function(){
+    $("#search_btn").click(function () {
         var search = $("#search_input").val();
-        location.href = "/org/index.html?search="+search+"&p=1";
+        location.href = "/org/index.html?search=" + search + "&p=1";
     })
 
     // 获取近期文章 top5
     $.ajax({
         url: app_root + "/essay/details",
-        type:"get",
-        dataType:"json",
-        data:{
-            list_for:"recent",
+        type: "get",
+        dataType: "json",
+        data: {
+            list_for: "recent",
         },
         headers: {
             "X-CSRFToken": get_csrf_token(),
         },
-    }).done(function(msg){
-        if(msg.state=="ok"){
+    }).done(function (msg) {
+        if (msg.state == "ok") {
             // console.log(msg.msg);
-            var html="";
-            for(var i=0; i<msg.msg.length;i++){
-                html+="<li><a href=\"/org/essay.html?id="+msg.msg[i].id+"\"><span>"+xss_defender(msg.msg[i].title)+'</span>&nbsp;<i><svg t="1642753378290" class="icon" viewbox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5548" width="18" height="18"><path d="M510.557138 204.354248c-302.839283 0-458.323497 306.150702-458.323497 306.150702s117.330242 306.189587 458.323497 306.189587c308.802088 0 458.300984-304.995389 458.300984-304.995389S818.167075 204.354248 510.557138 204.354248L510.557138 204.354248 510.557138 204.354248zM511.245823 701.866279c-110.729917 0-190.772928-83.72589-190.772928-191.364399 0-107.647719 80.049151-191.352119 190.772928-191.352119 110.723777 0 190.763718 83.697237 190.763718 191.352119C702.010565 618.140389 621.970624 701.866279 511.245823 701.866279L511.245823 701.866279 511.245823 701.866279zM511.245823 395.675668c-63.286372 0.145309-114.460892 53.321416-114.460892 114.827235 0 61.473073 51.175543 114.821095 114.460892 114.821095 63.282279 0 114.453728-53.352115 114.453728-114.821095C625.703645 448.975595 574.529125 395.556964 511.245823 395.675668L511.245823 395.675668 511.245823 395.675668z" p-id="5549" fill="#bfbfbf"></path></svg></i>'+
-                "&nbsp;<span>"+msg.msg[i].watch_num+"</span></a></li>";
+            var html = "";
+            for (var i = 0; i < msg.msg.length; i++) {
+                html += "<li><a href=\"/org/essay.html?id=" + msg.msg[i].id + "\"><span>" + xss_defender(msg.msg[i].title) + '</span>&nbsp;<i><svg t="1642753378290" class="icon" viewbox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5548" width="18" height="18"><path d="M510.557138 204.354248c-302.839283 0-458.323497 306.150702-458.323497 306.150702s117.330242 306.189587 458.323497 306.189587c308.802088 0 458.300984-304.995389 458.300984-304.995389S818.167075 204.354248 510.557138 204.354248L510.557138 204.354248 510.557138 204.354248zM511.245823 701.866279c-110.729917 0-190.772928-83.72589-190.772928-191.364399 0-107.647719 80.049151-191.352119 190.772928-191.352119 110.723777 0 190.763718 83.697237 190.763718 191.352119C702.010565 618.140389 621.970624 701.866279 511.245823 701.866279L511.245823 701.866279 511.245823 701.866279zM511.245823 395.675668c-63.286372 0.145309-114.460892 53.321416-114.460892 114.827235 0 61.473073 51.175543 114.821095 114.460892 114.821095 63.282279 0 114.453728-53.352115 114.453728-114.821095C625.703645 448.975595 574.529125 395.556964 511.245823 395.675668L511.245823 395.675668 511.245823 395.675668z" p-id="5549" fill="#bfbfbf"></path></svg></i>' +
+                    "&nbsp;<span>" + msg.msg[i].watch_num + "</span></a></li>";
             }
-        }else{
-            html+="<li>出现错误...</li>"
+        } else {
+            html += "<li>出现错误...</li>"
         }
         $("#recent_essay").html(html);
-        
-    }).fail(function(e){
+
+    }).fail(function (e) {
         console.log(e)
     })
 
     // 获取近期文章 top5
     $.ajax({
         url: app_root + "/essay/details",
-        type:"get",
-        dataType:"json",
-        data:{
-            list_for:"hot",
+        type: "get",
+        dataType: "json",
+        data: {
+            list_for: "hot",
         },
         headers: {
             "X-CSRFToken": get_csrf_token(),
         },
-    }).done(function(msg){
-        if(msg.state=="ok"){
+    }).done(function (msg) {
+        if (msg.state == "ok") {
             // console.log(msg.msg);
-            var html="";
-            for(var i=0; i<msg.msg.length;i++){
-                html+="<li><a href=\"/org/essay.html?id="+msg.msg[i].id+"\"><span>"+xss_defender(msg.msg[i].title)+'</span>&nbsp;<i><svg t="1642753378290" class="icon" viewbox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5548" width="18" height="18"><path d="M510.557138 204.354248c-302.839283 0-458.323497 306.150702-458.323497 306.150702s117.330242 306.189587 458.323497 306.189587c308.802088 0 458.300984-304.995389 458.300984-304.995389S818.167075 204.354248 510.557138 204.354248L510.557138 204.354248 510.557138 204.354248zM511.245823 701.866279c-110.729917 0-190.772928-83.72589-190.772928-191.364399 0-107.647719 80.049151-191.352119 190.772928-191.352119 110.723777 0 190.763718 83.697237 190.763718 191.352119C702.010565 618.140389 621.970624 701.866279 511.245823 701.866279L511.245823 701.866279 511.245823 701.866279zM511.245823 395.675668c-63.286372 0.145309-114.460892 53.321416-114.460892 114.827235 0 61.473073 51.175543 114.821095 114.460892 114.821095 63.282279 0 114.453728-53.352115 114.453728-114.821095C625.703645 448.975595 574.529125 395.556964 511.245823 395.675668L511.245823 395.675668 511.245823 395.675668z" p-id="5549" fill="#bfbfbf"></path></svg></i>'+
-                "&nbsp;<span>"+msg.msg[i].watch_num+"</span></a></li>";
+            var html = "";
+            for (var i = 0; i < msg.msg.length; i++) {
+                html += "<li><a href=\"/org/essay.html?id=" + msg.msg[i].id + "\"><span>" + xss_defender(msg.msg[i].title) + '</span>&nbsp;<i><svg t="1642753378290" class="icon" viewbox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5548" width="18" height="18"><path d="M510.557138 204.354248c-302.839283 0-458.323497 306.150702-458.323497 306.150702s117.330242 306.189587 458.323497 306.189587c308.802088 0 458.300984-304.995389 458.300984-304.995389S818.167075 204.354248 510.557138 204.354248L510.557138 204.354248 510.557138 204.354248zM511.245823 701.866279c-110.729917 0-190.772928-83.72589-190.772928-191.364399 0-107.647719 80.049151-191.352119 190.772928-191.352119 110.723777 0 190.763718 83.697237 190.763718 191.352119C702.010565 618.140389 621.970624 701.866279 511.245823 701.866279L511.245823 701.866279 511.245823 701.866279zM511.245823 395.675668c-63.286372 0.145309-114.460892 53.321416-114.460892 114.827235 0 61.473073 51.175543 114.821095 114.460892 114.821095 63.282279 0 114.453728-53.352115 114.453728-114.821095C625.703645 448.975595 574.529125 395.556964 511.245823 395.675668L511.245823 395.675668 511.245823 395.675668z" p-id="5549" fill="#bfbfbf"></path></svg></i>' +
+                    "&nbsp;<span>" + msg.msg[i].watch_num + "</span></a></li>";
             }
-        }else{
-            html+="<li>出现错误...</li>"
+        } else {
+            html += "<li>出现错误...</li>"
         }
         $("#hot_essay").html(html);
-        
-    }).fail(function(e){
+
+    }).fail(function (e) {
         console.log(e)
     })
 
